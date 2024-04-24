4b830cd224f10a4db2cd509092c98be8bf1c57aa
davidboll <david.boll@me.com>
2024-04-24 11:26:53 +0200

    Adds styling to header, removes boilerplate styling from app.css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Scrumguide</h1>
+    <div className={styles.header}>
+      <h1 className={styles.title}>Scrumguide</h1>
     </div>
   );
 };
