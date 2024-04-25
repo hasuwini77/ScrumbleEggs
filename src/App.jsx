b9fc887748b6092f405f7cbb6a91c7f6096c0d22
eri.mari <erimarilin@outlook.com>
2024-04-25 11:15:24 +0200

    navBar imported from nextUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import ScrumNavBar from "./components/Navbar";
+
 function App() {
   return (
     <>
+      <ScrumNavBar/>
         <h1 className="text-3xl text-center font-bold underline">
       Hello world!
     </h1>
