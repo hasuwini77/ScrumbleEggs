f47c4900fde0b486fb13336a85e4f12f09ff9646
eri.mari <erimarilin@outlook.com>
2024-04-25 15:08:29 +0200

    added datafolder/datafile

---
+++ src/data/data.js
@@ -0,0 +1,70 @@
+const Articels = [
+    {
+        key: 1,
+        header: 'Welcome to our page',
+        section: [
+            {
+                text: 'Welcome to our school project and our guide on Agile/Scrum methodology. Here you can explore information about the origin and how to applie in practice. Explore our work in how we learned the diffrent way of working and aply it on our work. Since we started we learned...blalbabla(more words coming)', 
+                text2: 'Happy learning! ',
+            }
+        ]
+    },
+    {
+        key: 2,
+        header: 'Implementing Scrum',
+        section: [
+            {
+                title: 'Getting Started',
+                subTitle: 'What is Scrum',
+                text: '',
+                text2: '',
+                subTitle2: 'The three Scrum Artifacts',
+                text3: '',
+            }
+        ]
+
+    },
+    {
+        key: 3,
+        header: 'Scrum Ceremonies',
+        section: [
+            {
+                title: 'Daily Stand-Ups',
+                subTitle: 'What is a stand-up meeting, in Scrum',
+                text: '',
+                text2: '',
+                subTitle2: 'Sprint planing',
+                text3: '',
+                subTitle3: 'Sprint Review',
+                text4: '',
+                subTitle4: 'Retrospective',
+                text5: '',
+            }
+        ]
+    },
+    {
+        key: 4,
+        header: 'Agile',
+        section: [
+            {
+                title: 'Definition of Agile',
+                text: 'The Agile "Software Development" Manifesto emerged from a meeting held on February 11-13, 2001, at The Lodge at Snowbird ski resort in Utah. It represents a collaborative effort among various software development methodologies, including Extreme Programming, SCRUM, DSDM, Adaptive Software Development, Crystal, Feature-Driven Development, and Pragmatic Programming. The term "agile" was chosen, although some participants had concerns about its pronunciation. The Agile Alliance, formed by independent thinkers in software development, adopted the Manifesto as a guiding principle. (link to Agile manifesto) ',
+                subTitle: 'History and origins',
+                text2: 'The meeting at Snowbird ski resort was preceded by a gathering organized by Kent Beck at the Rogue River Lodge in Oregon in the spring of 2000, which primarily focused on Extreme Programming proponents and a few outsiders. Discussions about "Light" or "Lightweight" methodologies took place throughout 2000, with proponents like Bob Martin advocating for a conference to bring together leaders in the field. Alistair Cockburn expressed dissatisfaction with the term "Light" due to its connotations. Eventually, a meeting was arranged in January to February 2001 in Chicago, leading to debates over the location, with Snowbird, Utah, being chosen. The Agile Alliance aims to promote innovative thinking in software development methodologies and organizations, encouraging agility in approach and mindset ',
+                subTitle2: 'Key Principles',
+                principle1: 'The highest priority is to satisfy the customer through early and continuous delivery of valuable software.', 
+                principle2: 'Welcome changing requirements, even late in development. Agile processes harness change for the customer’s competitive advantage', 
+                principle3: 'Deliver working software frequently, from a couple of weeks to a couple of months, with a preference to the shorter timescale.', 
+                principle4: 'Business people and developers must work together daily throughout the project.', 
+                principle5: 'Build projects around motivated individuals. Give them the environment and support they need and trust them to get the job done.', 
+                principle6: 'The most efficient and effective method of conveying information to and within a development team is face-to-face conversation.', 
+                principle7: 'Working software is the primary measure of progress.', 
+                principle8: 'Agile processes promote sustainable development. The sponsors, developers, and users should be able to maintain a constant pace indefinitely.', 
+                principle9: 'Continuous attention to technical excellence and good design enhances agility.', 
+                principle10: 'Simplicity – the art of maximizing the amount of work not done—is essential.', 
+                principle11: 'The best architectures, requirements, and designs emerge from self-organizing teams.', 
+                principle12: 'At regular intervals, the team reflects on how to become more effective, then tunes and adjusts its behavior accordingly.',
+            }
+        ]
+    }
+]
