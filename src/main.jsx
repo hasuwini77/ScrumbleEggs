8e9b6751a960c25ee60d4e21f689fbe0eefbd896
Edwin Andermyr <andermyredwin@gmail.com>
2024-04-26 13:23:03 +0200

    added dark theme as default

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      <main className="light text-foreground bg-background">
+      <main className="dark text-foreground bg-background">
         <App />
       </main>
     </NextUIProvider>
