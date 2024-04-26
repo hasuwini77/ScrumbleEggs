ea41fca07fc13f34157113fc6e657f2b9369cc73
Edwin Andermyr <andermyredwin@gmail.com>
2024-04-26 13:21:37 +0200

    added the light theme classname as well as a main element on the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <main className="light text-foreground bg-background">
+        <App />
+      </main>
     </NextUIProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
