dbd518186e190fc81592bef00a02afe38f05e4ee
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-04-27 15:46:05 +0200

    added some css

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Hero() {
     return (
-      <section className="bg-hero flex flex-col items-center mt-6 lg:mt-20">
+      <section className="hero flex flex-col items-center">
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
           Scrum Guide{" "}
           <span className="bg-gradient-to-r from-blue-500 to-red-800 text-transparent bg-clip-text">
