81d8a4a6ba3eaba263cae5372271d1dd7580b5bd
MartinAhlsen <martin.ahlsen@gmail.com>
2024-04-30 10:28:40 +0200

    add basic accordion menu

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ function App() {
         <ButtonComponent text="button1" className="px-5 py-5 ml-3 mt-3" />
         <ButtonComponent text="button2" className="px-5 py-5 ml-3 mt-3" />
       </div>
-      <SocialMediaBar/>
       <div className="accordeon w-full md:w-[300px]">
         <AccordeonMenu/>
       </div>
+      <SocialMediaBar/>
     </>
   );
 }
