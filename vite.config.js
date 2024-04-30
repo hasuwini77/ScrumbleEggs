82b5465e671946643a9c94f0b1e2c535cb34216c
Edwin Andermyr <andermyredwin@gmail.com>
2024-04-30 17:11:29 +0200

    fixed the vite config mistake

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const { nextui } = require("@nextui-org/react");
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      screens: {
-        xs: "480px",
-        ss: "620px",
-        sm: "768px",
-        md: "1060px",
-        lg: "1200px",
-        xl: "1700px",
-      },
-      linearGradientColors: (theme) => ({
-        "dark-purple": [theme("colors.dark-purple.DEFAULT"), theme("colors.dark-purple.primary.900")],
-      }),
-    },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
