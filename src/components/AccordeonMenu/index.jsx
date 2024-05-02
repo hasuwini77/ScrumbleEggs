1026dcafdfca441722d311919bb1cfb4df7b43bd
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-02 17:30:55 +0200

    trying to find a logic, currently ongoing step1

--- src/components/AccordeonMenu/index.jsx
+++ src/components/AccordeonMenu/index.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-import {Accordion, AccordionItem} from "@nextui-org/react";
+import { Accordion, AccordionItem } from "@nextui-org/react";
 
-export default function AccordeonMenu() {
-  const defaultContent =
-    "Submenu";
+export default function AccordeonMenu({ updateContent }) {
+  const defaultContent = "Submenu";
+  const chapter1 = "This is the Chapter 1";
+  const chapter2 = "This is the Chapter 2";
+  const chapter3 = "This is the Chapter 3";
+
+  const handleClick = (e, chapter) => {
+    let element = e.currentTarget;
+    element.classList.toggle("active");
+    updateContent(chapter);
+  };
 
   return (
     <Accordion>
-      <AccordionItem key="1" aria-label="Accordion 1" title="Accordion 1">
-        {defaultContent}
+      <AccordionItem key="1" aria-label="Accordion 1" title="Chapter 1">
+        <span className="cursor-pointer" onClick={() => handleClick(e, 1)}>
+          {chapter1}{" "}
+        </span>
       </AccordionItem>
-      <AccordionItem key="2" aria-label="Accordion 2" title="Accordion 2">
-        {defaultContent}
+      <AccordionItem key="2" aria-label="Accordion 2" title="Chapter 2" onPress={handleClick}>
+        <span className="cursor-pointer" onClick={() => handleClick(e, 2)}>
+          {chapter2}{" "}
+        </span>
       </AccordionItem>
-      <AccordionItem key="3" aria-label="Accordion 3" title="Accordion 3">
-        {defaultContent}
+      <AccordionItem key="3" aria-label="Accordion 3" title="Chapter 3">
+        <span className="cursor-pointer" onClick={() => handleClick(e, 3)}>
+          {chapter3}{" "}
+        </span>
       </AccordionItem>
     </Accordion>
   );
--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import AccordeonMenu from "../AccordeonMenu";
 const MainContent = () => {
   const [content, setContent] = useState("welcome");
-  const handleClick = () => {
-    setContent();
+  const updateContent = (chapter) => {
+    setContent(chapter);
   };
 
   const welcomeMessage = "This is the Welcome page";
   return (
     <>
       <div className="accordeon w-full md:w-[300px]">
-        <AccordeonMenu />
+        <AccordeonMenu updateContent={updateContent} />
       </div>
       <div className="md:p-4"> {welcomeMessage}</div>
     </>
