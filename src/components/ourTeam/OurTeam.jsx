e61128828c3cf0e93df92430048a0fd80e0350db
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-05-02 20:20:50 +0200

    some changes

--- src/components/ourTeam/OurTeam.jsx
+++ src/components/ourTeam/OurTeam.jsx
@@ -79,7 +79,7 @@ function OurTeam() {
                 <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg" className={styles.teamImg} alt="pic" />
                 <h3>JOHNATHAN HAWKINS</h3>
                 <div className={styles.teamInfo}><p>Head of SEO</p></div>
-                <p>Johnathan is our co-founder and has developed search strategies for a variety of clients from international brands to medium-sized businesses for over five years.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolore quis quod, assumenda exercitationem eveniet, quia iste impedit fugit Laboriosam, voluptatum?</p>
                 <ul className={styles.teamIcon}>
                   <li><a href="#" className={styles.twitter}><i className="fa fa-twitter"></i></a></li>
                   <li><a href="#" className={styles.pinterest}><i className="fa fa-pinterest"></i></a></li>
@@ -93,7 +93,7 @@ function OurTeam() {
                 <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team2a.jpg" className={styles.teamImg} alt="pic" />
                 <h3>ALEXANDRA SMITHS</h3>
                 <div className={styles.teamInfo}><p>SEO Specialist</p></div>
-                <p>Graduating with a degree in Spanish and English, Alexandra has always loved writing and now she’s lucky enough to do it as part of her new job inside our agency.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolore quis quod, assumenda exercitationem eveniet, quia iste impedit fugit Laboriosam, voluptatum?</p>
                 <ul className={styles.teamIcon}>
                   <li><a href="#" className={styles.twitter}><i className="fa fa-twitter"></i></a></li>
                   <li><a href="#" className={styles.pinterest}><i className="fa fa-pinterest"></i></a></li>
@@ -107,7 +107,7 @@ function OurTeam() {
                 <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team3a.jpg" className={styles.teamImg} alt="pic" />
                 <h3>ELISA JOHANSON</h3>
                 <div className={styles.teamInfo}><p>Marketing Manager</p></div>
-                <p>Elisa first fell in love with digital marketing at the university. He loves to learn and looks forward to being part of this new exciting industry for many years.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolore quis quod, assumenda exercitationem eveniet, quia iste impedit fugit Laboriosam, voluptatum?</p>
                 <ul className={styles.teamIcon}>
                   <li><a href="#" className={styles.twitter}><i className="fa fa-twitter"></i></a></li>
                   <li><a href="#" className={styles.pinterest}><i className="fa fa-pinterest"></i></a></li>
