f1232256bcc8e330d3430ebee3af5a39517c5194
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-02 13:48:19 +0200

    added styled

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 const MainContent = () => {
+  const [content, setContent] = useState("welcome");
+  const handleClick = () => {};
   return (
     <>
       <div className="md:min-w-[400px] mr-3 p-4">
         THis is Sidebar
-        <p className="p-4 cursor-pointer hover:bg-slate-300"> Article 1 </p> <p className="p-4 cursor-pointer  hover:bg-slate-300"> Article 2 </p>
+        <p className="p-4 cursor-pointer hover:bg-slate-300" onClick={handleClick}>
+          Article 1
+        </p>
+        <p className="p-4 cursor-pointer  hover:bg-slate-300" onClick={handleClick}>
+          Article 2
+        </p>
       </div>
       <div className="md:p-4"> This is Welcome message dude</div>
     </>
