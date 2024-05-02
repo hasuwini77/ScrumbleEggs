b1c8cbd1aab56c1b1d1a84c8adb8559d55d996cf
eri.mari <erimarilin@outlook.com>
2024-05-02 13:33:46 +0200

    added AgileContent

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import ScrumNavBar from "./components/Navbar";
 import ButtonComponent from "./components/Button";
-
+import Agile from "./components/Agile";
 function App() {
   return (
     <>
@@ -10,6 +10,7 @@ function App() {
         <ButtonComponent text="button1" className="px-5 py-5 ml-3 mt-3" />
         <ButtonComponent text="button2" className="px-5 py-5 ml-3 mt-3" />
       </div>
+      <Agile/>
     </>
   );
 }
