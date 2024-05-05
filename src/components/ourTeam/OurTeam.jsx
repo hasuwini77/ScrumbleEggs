21b4d8243ef777bad2c608f4495f9e7295af59f2
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-05-05 16:26:13 +0200

    added social icons

--- src/components/ourTeam/OurTeam.jsx
+++ src/components/ourTeam/OurTeam.jsx
@@ -1,4 +1,4 @@
-import styles from './OurTeam.module.css';
+/* import styles from './OurTeam.module.css';
 
 function OurTeam() {
   let message = "Check out our team members who made this Scrum Guide better than ever!"
@@ -82,6 +82,44 @@ function OurTeam() {
                 </ul>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default OurTeam;
+ */
+
+
+import styles from './ourTeam.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faPinterest, faFacebook, faDribbble } from '@fortawesome/free-brands-svg-icons';
+
+function OurTeam() {
+  let message = `There are many variations of passages of Lorem Ipsum available but the \n majority have suffered alteration in some injected humour.`;
+
+  return (
+    <>
+      <section className={styles.sectionWhite}>
+        <div className="container mx-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
+            <div className="col-span-1">
+              <div className={styles.teamItem}>
+                <img src="https://demo.epic-webdesign.com/tf-pacifico/v1/images/team1a.jpg" className={styles.teamImg} alt="pic" />
+                <h3>JOHNATHAN HAWKINS</h3>
+                <div className={styles.teamInfo}><p>Head of SEO</p></div>
+                <p>Johnathan is our co-founder and has developed search strategies for a variety of clients from international brands to medium-sized businesses for over five years.</p>
+                <ul className={styles.teamIcon}>
+                  <li><a href="#" className={`${styles.twitter}`}><FontAwesomeIcon icon={faTwitter} /></a></li>
+                  <li><a href="#" className={`${styles.pinterest}`}><FontAwesomeIcon icon={faPinterest} /></a></li>
+                  <li><a href="#" className={`${styles.facebook}`}><FontAwesomeIcon icon={faFacebook} /></a></li>
+                  <li><a href="#" className={`${styles.dribble}`}><FontAwesomeIcon icon={faDribbble} /></a></li>
+                </ul>
+              </div>
+            </div>
+            {/* Other team members */}
           </div>
         </div>
       </section>
