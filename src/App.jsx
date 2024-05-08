73bf575e3647606c180dbfb01887dded8cdbec90
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-08 15:14:11 +0200

    merged the css styling ed's branch hope it still works as usual

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import ScrumNavBar from "./components/Navbar";
 import ButtonComponent from "./components/Button";
-<<<<<<< HEAD
 import Hero from "./components/Hero";
-=======
-import OurTeam from "./components/OurTeam/OurTeam";
-import MainContent from "./components/MainContent";
->>>>>>> feature/KAN-78-CSS-DisplayedContent
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
 import OurTeam from "./components/OurTeam/OurTeam";
@@ -23,9 +18,7 @@ function App() {
         <ButtonComponent text="button1" className="px-5 py-5 ml-3 mt-3" />
         <ButtonComponent text="button2" className="px-5 py-5 ml-3 mt-3" />
       </div>
-
       <MainContent />
-
       <HighlightedFeature title="Feature Title" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris." images={[feature1, feature2]} />
       <OurTeam />
       <Footer />
