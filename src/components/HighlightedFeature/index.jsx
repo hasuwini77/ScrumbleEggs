a3073cf33efb7fe2c90072ee3ee0bc3d2e4aceac
davidboll <david.boll@me.com>
2024-05-08 10:03:49 +0200

    Removes text-color class

--- src/components/HighlightedFeature/index.jsx
+++ src/components/HighlightedFeature/index.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 const HighlightedFeature = ({ title, text, images }) => {
   return (
     <div className="max-w-4xl mx-auto p-5 text-center">
-      <h2 className="mb-2 text-2xl font-bold text-white">{title}</h2>
-      <p className="mb-4 text-white">{text}</p>
+      <h2 className="mb-2 text-2xl font-bold">{title}</h2>
+      <p className="mb-4">{text}</p>
       <div
         className={`flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4 ${
           images.length > 1 ? "md:justify-between" : "justify-center"
