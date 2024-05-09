c376f5e2ed59a22790364f4cf1482619e95bfd93
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-05-09 12:39:09 +0200

    added profile pics

--- src/components/ourTeam/OurTeam.jsx
+++ src/components/ourTeam/OurTeam.jsx
@@ -1,8 +1,10 @@
 import styles from './OurTeam.module.css';
 import OurTeamCard from '../OurTeamCard/index';
 import avatar from '../../assets/images/david-profile.png';
+import avatar2 from '../../assets/images/erika-profile.jpg';
 import avatar3 from '../../assets/images/edwin-profile.jpeg';
 import avatar4 from '../../assets/images/noah-profile.jpg';
+import avatar5 from '../../assets/images/martin-profile.jpg';
 
 function OurTeam() {
   let message = "Check out our team members who made this Scrum Guide!"
@@ -17,10 +19,10 @@ function OurTeam() {
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4">
             <OurTeamCard name="David Boll" role="Scrum Master" image={avatar} />
-            <OurTeamCard name="Erika Lundström" role="Developer" />
+            <OurTeamCard name="Erika Lundström" role="Developer" image={avatar2} />
             <OurTeamCard name="Edwin Andermyr" role="Developer" image={avatar3}/>
             <OurTeamCard name="Noah Gordon" role="Devloper" image={avatar4} />
-            <OurTeamCard name="Martin Ahlsén" role="Developer" />
+            <OurTeamCard name="Martin Ahlsén" role="Developer" image={avatar5} />
           </div>
         </div>
       </section>
