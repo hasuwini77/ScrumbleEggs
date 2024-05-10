1bb6b91f5b471f667d4ef8165da9e3c05b3a1ad8
Noah Gordon <noah.gordon@edu.cmeducations.se>
2024-05-10 11:42:48 +0200

    added buttonComponent

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,25 +1,29 @@
 import ButtonComponent from "../Button";
 import styles from "./Hero.module.css";
-import { Button } from "@nextui-org/react";
 
-function Hero({ isDarkMode, text, className }) {
+function Hero({ isDarkMode }) {
   return (
     <>
       <section className={`${styles.hero} flex flex-col items-center py-10 ss:py-20 sm:py-36`} style={{ backgroundImage: isDarkMode ? "var(--hero-background-dark)" : "var(--hero-background-light)" }}>
         <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
           Scrum Guide <span className="bg-gradient-to-r from-blue-500 to-red-800 text-transparent bg-clip-text">for developers</span>
         </h1>
-        <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">Empower your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality!</p>
+        <p className="mt-10 text-lg text-center text-black-500 max-w-4xl">What it is, how it works, and how to start</p>
         <div className="flex justify-center my-10">
-{/*           <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
-            Learn Scrum
-          </a>
-          <a href="#" className="py-3 px-4 mx-3 rounded-md border">
-            Documentation
-          </a> */}
-
-          <ButtonComponent text="Learn SCruM" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md" />
-          <ButtonComponent text="Document" className="py-3 px-4 mx-3 rounded-md border" />
+          <ButtonComponent
+            text="Learn Scrum"
+            className={
+              isDarkMode
+                ? "bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md dark:text-white"
+                : "bg-gradient-to-r from-blue-500 to-red-800 py-3 px-4 mx-3 rounded-md"
+            } />
+          <ButtonComponent
+            text="Our Team"
+            className={
+              isDarkMode
+                ? "bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md dark:text-white"
+                : "bg-gradient-to-r from-blue-500 to-red-800 py-3 px-4 mx-3 rounded-md"
+            } />
         </div>
       </section>
     </>
