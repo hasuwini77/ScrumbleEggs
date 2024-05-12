4863449f74e7fb9ed9d0512d7b5720e0b5203f4c
Edwin Andermyr <andermyredwin@gmail.com>
2024-05-12 16:51:04 +0200

    fixed the path of outteam.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ButtonComponent from "./components/Button";
 import Hero from "./components/Hero/index.jsx";
 import Footer from "./components/Footer";
 import MainContent from "./components/MainContent";
-import OurTeam from "./components/OurTeam/OurTeam";
+import OurTeam from "./components/OurTeam/OurTeam.jsx";
 import HighlightedFeature from "./components/HighlightedFeature";
 import feature1 from "./assets/images/feature1.png";
 import feature2 from "./assets/images/feature2.png";
